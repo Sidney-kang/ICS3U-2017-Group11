be12b920429d3c0b2a98ccf45f374a73f0b2fd59
Sidney <sidney.kang@ocsbstudent.ca>
2018-01-03 20:32:22 -0500

    Credits and main menu scene

--- credits_scene.py
+++ credits_scene.py
@@ -1,21 +1,59 @@
-# Created by: Shuvaethy Neill
-# Created on: Dec 2017
+# Created by: Mr. Coxall
+# Created on: Sep 2016
 # Created for: ICS3U
-# This scene shows the credits scene
+# This scene shows the main menu.
 
 from scene import *
 import ui
 
-
-class MainMenuScene(Scene):
+class CreditsScene(Scene):
     def setup(self):
         # this method is called, when user moves to this scene
         
-        # add background color
-        self.background = SpriteNode(position = self.size / 2, 
-                                     color = 'white', 
+        self.score_position = Vector2()
+        self.size_of_screen_x = self.size.x
+        self.size_of_screen_y = self.size.y
+        self.center_of_screen_x = self.size_of_screen_x/2
+        self.center_of_screen_y = self.size_of_screen_y/2 
+        self.scale_size = 0.4
+        
+        # add blue sky background 
+        self.background = SpriteNode('./assets/sprites/main_menu_background.PNG',
+        	                           position = self.size / 2, 
                                      parent = self, 
                                      size = self.size)
+                                     
+        bush_position = Vector2()
+        bush_position.y = (self.size_of_screen_y - (2 * (self.center_of_screen_y))) + 100
+        bush_position.x = self.size_of_screen_x - 100                           
+        self.bush = SpriteNode('./assets/sprites/bush.PNG',
+                                       parent = self, 
+                                       position = bush_position,
+                                       scale = 0.45)       
+                                       
+        bush_2_position = Vector2()
+        bush_2_position.y = (self.size_of_screen_y - (2 * (self.center_of_screen_y))) + 100
+        bush_2_position.x = (self.size_of_screen_x - (2 * (self.center_of_screen_x))) + 100                               
+        self.bush_2 = SpriteNode('./assets/sprites/bush.PNG',
+                                       parent = self, 
+                                       position = bush_2_position,
+                                       scale = 0.45)                                                                                                                                                                                                                     
+                                                       
+        credits_title_position = Vector2()
+        credits_title_position.y = self.center_of_screen_y + 270
+        credits_title_position.x = self.center_of_screen_x                      
+        self.credits_title = SpriteNode('./assets/sprites/credits_title.PNG',
+                                       parent = self, 
+                                       position = credits_title_position,
+                                       scale = 0.45)   
+                                       
+        home_button_position = Vector2()
+        home_button_position.y = self.size_of_screen_y - 70
+        home_button_position.x = (self.size_of_screen_x - (2 * (self.center_of_screen_x))) + 100                   
+        self.home_button = SpriteNode('./assets/sprites/home_button.PNG',
+                                       parent = self, 
+                                       position = home_button_position,
+                                       scale = 0.25)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
     
     def update(self):
         # this method is called, hopefully, 60 times a second
@@ -32,7 +70,10 @@ class MainMenuScene(Scene):
     def touch_ended(self, touch):
         # this method is called, when user releases a finger from the screen
         pass
-    
+        
+        if self.home_button.frame.contains_point(touch.location):
+           self.dismiss_modal_scene()
+        
     def did_change_size(self):
         # this method is called, when user changes the orientation of the screen
         # thus changing the size of each dimension
@@ -47,4 +88,3 @@ class MainMenuScene(Scene):
         # this method is called, when user place app from background 
         # back into use. Reload anything you might need.
         pass
-    
--- main_menu_scene.py
+++ main_menu_scene.py
@@ -6,16 +6,67 @@
 from scene import *
 import ui
 
+from levels_scene import *
+from settings_scene import *
+from credits_scene import *
 
 class MainMenuScene(Scene):
     def setup(self):
         # this method is called, when user moves to this scene
         
-        # add background color
-        self.background = SpriteNode(position = self.size / 2, 
-                                     color = 'blue', 
+        self.score_position = Vector2()
+        self.size_of_screen_x = self.size.x
+        self.size_of_screen_y = self.size.y
+        self.center_of_screen_x = self.size_of_screen_x/2
+        self.center_of_screen_y = self.size_of_screen_y/2 
+        self.scale_size = 0.4
+        
+        # add blue sky background 
+        self.background = SpriteNode('./assets/sprites/main_menu_background.PNG',
+        	                           position = self.size / 2, 
                                      parent = self, 
                                      size = self.size)
+                                     
+        bush_position = Vector2()
+        bush_position.y = (self.size_of_screen_y - (2 * (self.center_of_screen_y))) + 100
+        bush_position.x = self.size_of_screen_x - 100                           
+        self.bush = SpriteNode('./assets/sprites/bush.PNG',
+                                       parent = self, 
+                                       position = bush_position,
+                                       scale = 0.45)       
+                                       
+        bush_2_position = Vector2()
+        bush_2_position.y = (self.size_of_screen_y - (2 * (self.center_of_screen_y))) + 100
+        bush_2_position.x = (self.size_of_screen_x - (2 * (self.center_of_screen_x))) + 100                               
+        self.bush_2 = SpriteNode('./assets/sprites/bush.PNG',
+                                       parent = self, 
+                                       position = bush_2_position,
+                                       scale = 0.45)                               
+                                     
+        start_button_position = Vector2()
+        start_button_position.y = self.center_of_screen_y - 100 
+        start_button_position.x = self.center_of_screen_x 
+        self.start_button = SpriteNode('./assets/sprites/start_button.PNG',
+                                       parent = self,
+                                       position = start_button_position,
+                                       scale = 0.3)        
+                                       
+        settings_button_position = Vector2()
+        settings_button_position.y = self.center_of_screen_y - 200
+        settings_button_position.x = self.center_of_screen_x 
+        self.settings_button = SpriteNode('./assets/sprites/settings_button.PNG',
+                                          parent = self,
+                                          position = settings_button_position,
+                                          scale = 0.3)      
+                                          
+        credits_button_position = Vector2()
+        credits_button_position.y = self.center_of_screen_y - 300
+        credits_button_position.x = self.center_of_screen_x 
+        self.credits_button = SpriteNode('./assets/sprites/credits_button.PNG',
+                                          parent = self,
+                                          position = credits_button_position,
+                                          scale = 0.3)                                                                                                                                         
+                                                                                                                                                                                                                                                                                                                                                            
     
     def update(self):
         # this method is called, hopefully, 60 times a second
@@ -32,6 +83,13 @@ class MainMenuScene(Scene):
     def touch_ended(self, touch):
         # this method is called, when user releases a finger from the screen
         pass
+        
+        if self.settings_button.frame.contains_point(touch.location):
+             self.present_modal_scene(SettingsScene())
+        if self.credits_button.frame.contains_point(touch.location):
+             self.present_modal_scene(CreditsScene())
+        if self.start_button.frame.contains_point(touch.location):
+             self.present_modal_scene(LevelsScene())
     
     def did_change_size(self):
         # this method is called, when user changes the orientation of the screen
@@ -47,4 +105,3 @@ class MainMenuScene(Scene):
         # this method is called, when user place app from background 
         # back into use. Reload anything you might need.
         pass
-    
