b5fee0ace8761bfaa2aac7fad109c5838ad03ef9
Sidney <sidney.kang@ocsbstudent.ca>
2018-01-07 18:25:49 -0500

    Lose scene

--- lose_scene.py
+++ lose_scene.py
@@ -4,18 +4,56 @@
 # This scene shows the losing scene.
 
 from scene import *
+import sound
 import ui
 
+from main_menu_scene import *
+
+#from settings_scene import character_gender
 
 class LoseScene(Scene):
     def setup(self):
         # this method is called, when user moves to this scene
         
+        self.score_position = Vector2()
+        self.size_of_screen_x = self.size.x
+        self.size_of_screen_y = self.size.y
+        self.center_of_screen_x = self.size_of_screen_x/2
+        self.center_of_screen_y = self.size_of_screen_y/2 
+     #   self.scale_size = 0.4
+        
         # add background color
-        self.background = SpriteNode(position = self.size / 2, 
-                                     color = 'blue', 
+        self.black = SpriteNode(position = self.size / 2, 
+                                color = ('black'), 
+                                parent = self, 
+                                size = self.size)
+                                      
+        # add lose scene background 
+     #  if character_gender == './assets/sprites/boy_thief.PNG':
+        background_position = Vector2()
+        background_position.y = self.center_of_screen_y + 100
+        background_position.x = self.center_of_screen_x                         
+        self.background = SpriteNode('./assets/sprites/lose_scene_background.PNG',
                                      parent = self, 
-                                     size = self.size)
+                                     position = background_position,
+                                     size = self.size/1.2) 
+                                     
+        home_button_position = Vector2()
+        home_button_position.y = self.size_of_screen_y - 70
+        home_button_position.x = (self.size_of_screen_x - (2 * (self.center_of_screen_x))) + 100                   
+        self.home_button = SpriteNode('./assets/sprites/home_button.PNG',
+                                       parent = self, 
+                                       position = home_button_position,
+                                       scale = 0.25)        
+                                       
+        lose_label_position = Vector2()   
+        lose_label_position.y = self.center_of_screen_y - 275
+        lose_label_position.x = self.center_of_screen_x                                           
+        self.lose = LabelNode(text = 'YOU LOSE',
+                             font = ('Marker Felt', 150),
+                             parent = self,
+                             color = '#ffffff',
+                             position = lose_label_position)                                                                                                                    
     
     def update(self):
         # this method is called, hopefully, 60 times a second
@@ -32,6 +70,10 @@ class LoseScene(Scene):
     def touch_ended(self, touch):
         # this method is called, when user releases a finger from the screen
         pass
+        
+        if self.home_button.frame.contains_point(touch.location):
+           sound.play_effect('8ve:8ve-tap-mellow')
+           self.present_modal_scene(MainMenuScene())      
     
     def did_change_size(self):
         # this method is called, when user changes the orientation of the screen
